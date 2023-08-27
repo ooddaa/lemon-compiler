e153c8a19b76d4f629d1b59022b9bfad9b5e883c
oda <ooddaa@gmail.com>
2023-08-27 10:08:21 +0100

    [native_code] pass w/wo [native] option
    If you add the flag native to the compilation, and you have a HiPE enabled runtime system, then the compiler will generate native code for your module and store the native code along with the beam code in the .beam. file.

--- erlang/world.erl
+++ erlang/world.erl
@@ -1,4 +1,8 @@
--module(hehe).
+% up until final pass (Native code), 
+% compiler didn't mind discrepancies 
+% between module name and file name
+% -module(hehe). 
+-module(world). 
 -export([hello/0]).
 
 -include("world.hrl").
